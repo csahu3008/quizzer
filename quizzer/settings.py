095833d20d70ddff67b1b4f23c4c452e72563d8e
Chandra Shekhar <csahu3932@gmail.com>
2019-09-04 19:54:20 +0530

    changes

--- quizzer/settings.py
+++ quizzer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -83,6 +83,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
 }
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
@@ -124,4 +127,4 @@ AUTH_USER_MODEL='users.Quizzers'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'quizzer/static'),]
