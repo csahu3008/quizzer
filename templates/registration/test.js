63ef39812360981586c7bd6260fa97f2b36906e7
Chandra Shekhar <csahu3932@gmail.com>
2022-04-25 18:07:21 +0530

    modified

--- templates/registration/test.js
+++
@@ -1,5 +0,0 @@
-$('#contributer').on('click',function(){
-    var $inp6=$("<input>",{type:'button',id:'contributerbtn',class:'btn btn-success',value:'Contributer'})
-      $('#forms-section').html("{{form.as_p|linebreaks}}").append($inp6)
-      
-   });
